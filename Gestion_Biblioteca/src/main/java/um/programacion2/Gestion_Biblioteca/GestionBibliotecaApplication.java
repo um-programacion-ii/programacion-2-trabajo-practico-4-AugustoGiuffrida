a45fd34540317e2f2b5e3acb53030892aab8e853
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2025-05-07 11:29:49 -0300

    Configurar proyecto Spring Boot

---
+++ Gestion_Biblioteca/src/main/java/um/programacion2/Gestion_Biblioteca/GestionBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package um.programacion2.Gestion_Biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionBibliotecaApplication.class, args);
+	}
+
+}
