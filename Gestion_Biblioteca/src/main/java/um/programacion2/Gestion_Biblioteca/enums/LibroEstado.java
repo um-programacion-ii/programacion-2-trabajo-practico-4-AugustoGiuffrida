bf9377cc4caae3603934363c8c54e7b7b20f6b86
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2025-05-07 13:18:32 -0300

    Implementar clases libro, usuario, prestamo

---
+++ Gestion_Biblioteca/src/main/java/um/programacion2/Gestion_Biblioteca/enums/LibroEstado.java
@@ -0,0 +1,7 @@
+package um.programacion2.Gestion_Biblioteca.enums;
+
+public enum LibroEstado {
+    DISPONIBLE,
+    PRESTADO,
+    EN_REPARACION,
+}
---
+++ Gestion_Biblioteca/src/main/java/um/programacion2/Gestion_Biblioteca/modelos/Usuario.java
@@ -0,0 +1,15 @@
+package um.programacion2.Gestion_Biblioteca.modelos;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import um.programacion2.Gestion_Biblioteca.enums.UsuarioEstado;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    private Long id;
+    private String nombre;
+    private String email;
+    private UsuarioEstado estado;
+}
